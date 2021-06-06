7134fd8f193f478ce2352da9bc678b3ccbbfa82e
Languiller <Daniel.Languiller@team.telstra.com>
2021-06-06 22:36:02 +1000

    added hooks

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,14 +1,41 @@
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/CartContext";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnClassesOn, setBtnClassesOn] = useState(false);
+  const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((acc, el) => {
+    return acc + el.amount;
+  }, 0);
+
+  const btnClasses = `${classes.button} ${btnClassesOn ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnClassesOn(true);
+
+    const timer = setTimeout(() => {
+      setBtnClassesOn(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button onClick={props.onClick} className={classes.button}>
+    <button onClick={props.onClick} className={btnClasses}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
